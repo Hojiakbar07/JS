c255990e95600f7ef4a15c1721d92e744236b87a
Hojiakbar <ch@gmail.com>
2022-07-11 13:50:16 +0500

    js

---
+++ index.js
@@ -0,0 +1,27 @@
+var name = prompt('Введите имя:');
+console.log(name);
+
+var age = +prompt('Введите ваш возраст:');
+console.log(age);
+
+var plus = 25 + 3;
+var ask = +prompt('Задание-1: 25 + 3');
+console.log('25 + 3 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 13 - 7;
+var ask = +prompt('Задание-2: 13 - 7');
+console.log('13 - 7 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 3 * 5;
+var ask = +prompt('Задание-3: 3 * 5');
+console.log('3 * 5 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 32 / 4;
+var ask = +prompt('Задание-4: 32 / 4');
+console.log('32 / 4 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 12 % 5;
+var ask = +prompt('Задание-5: 12 % 5');
+console.log('12 % 5 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+alert('Перейдите в консоль разработчика и сверьте свои ответы :) ')
